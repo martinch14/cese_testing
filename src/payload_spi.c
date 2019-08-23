0f23f674e68ab9226b8a46b071a29fb95415c60c
Martin <martinch14@gmail.com>
2019-08-23 19:51:51 -0300

    add comentarios en payload_spi.c

--- src/payload_spi.c
+++ src/payload_spi.c
@@ -14,6 +14,8 @@
 /* === Inclusiones de cabeceras ================================================================ */
 #include "payload_spi.h"
 
+//Posibles operaciones { OPERACION_ESCRITURA, OPERACION_LECTURA} trinamicOperacion_t;
+//Segun la operacion seleccionada, devuelve un valor uint8_t que es el que representa a la operacion
 
 uint8_t trinamicSetOperacion(trinamicOperacion_t operacion){
     switch (operacion){
@@ -26,6 +28,8 @@ uint8_t trinamicSetOperacion(trinamicOperacion_t operacion){
     }
 }
 
+//posibles registros {TMC4671_PID_FLUX_P_FLUX_I,TMC4671_PID_TORQUE_P_TORQUE_I,TMC4671_PID_VELOCITY_P_VELOCITY_I,TMC4671_PID_POSITION_P_POSITION_I}trinamicRegistro_t;
+//Segun el registro recibido devuelve un valor que representa al registro
 
 uint8_t trinamicSetRegistro(trinamicRegistro_t registro){
     switch (registro){
@@ -42,11 +46,16 @@ uint8_t trinamicSetRegistro(trinamicRegistro_t registro){
     }
 }
 
+//Devuelve un valor uint8_t que contiene el registro sobre el cual se quiere operar y el tipo de operacion a realizar
+// La cantidad de registros sobre los que se pueden operar son 120, se usa del uint8_t solamente los primerios 7 bits
+// La operacion de escritura queda definida con un 1 en el bits mas significativo.
+// Por lo tanto en un byte se pueden representar todos los registros y el tipo de operacion a realizar
 
 uint8_t trinamicsetOperacionRegistroEscritura(trinamicOperacion_t operacion,trinamicRegistro_t registro){
         return trinamicSetOperacion(operacion)  + trinamicSetRegistro(registro);
 }
 
+//Esta funciona arma un datagrama de 40 bits con la operacion y el registro ya definido, y el valor que se le quiere setear
 
 void trinamicArmarDatagrama(uint8_t * datagrama, uint8_t operacionregistro, uint32_t valor ){
 
